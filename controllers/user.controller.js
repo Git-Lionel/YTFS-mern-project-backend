91e7c9121cc2e9e06ac9424d36fc0a01962cdb0e
Git-Lionel <lionelhue@gmail.com>
2022-07-30 23:19:18 +0200

    Sauvegarde à 1h24'58''

---
+++ controllers/user.controller.js
@@ -0,0 +1,43 @@
+const UserModel = require("../models/user.model");
+const ObjectID = require("mongoose").Types.ObjectId;
+
+module.exports.getAllUsers = async (req, res) => {
+  const users = await UserModel.find().select("-password");
+  res.status(200).json(users);
+};
+
+module.exports.userInfo = (req, res) => {
+  if (!ObjectID.isValid(req.params.id))
+    return res.status(400).send("ID unkown : " + req.params.id);
+
+  UserModel.findById(req.params.id, (err, docs) => {
+    if (!err) res.send(docs);
+    else console.log("ID unkown : " + err);
+  }).select("-password");
+};
+
+module.exports.updateUser = async (req, res) => {
+  console.log(req.params.id);
+  console.log(req.body.bio);
+  if (!ObjectID.isValid(req.params.id))
+    return res.status(400).send("ID unkown : " + req.params.id);
+
+  try {
+    await UserModel.findOneAndUpdate(
+      { _id: req.params.id },
+      {
+        $set: {
+          bio: req.body.bio,
+        },
+      },
+      { new: true, upsert: true, setDefaultsOnInsert: true },
+      (err, docs) => {
+        if (!err) return res.send(docs);
+        if (err) return res.status(500).send({ message: err });
+      }
+    );
+  } catch (err) {
+    console.log("titi");
+    return res.status(500).json({ message: err });
+  }
+};
