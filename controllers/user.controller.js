30cab55dc72cf5771c927e58bedf554c3adffda9
Lionel Hue <lionel.hue@sogeclairaerospace.com>
2022-07-31 21:31:15 +0200

    Sauvegarde à 2h33'48''

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -17,8 +17,6 @@ module.exports.userInfo = (req, res) => {
 };
 
 module.exports.updateUser = async (req, res) => {
-  console.log(req.params.id);
-  console.log(req.body.bio);
   if (!ObjectID.isValid(req.params.id))
     return res.status(400).send("ID unkown : " + req.params.id);
 
@@ -30,14 +28,27 @@ module.exports.updateUser = async (req, res) => {
           bio: req.body.bio,
         },
       },
-      { new: true, upsert: true, setDefaultsOnInsert: true },
-      (err, docs) => {
-        if (!err) return res.send(docs);
-        if (err) return res.status(500).send({ message: err });
-      }
+      { new: true, upsert: true, setDefaultsOnInsert: true }
+      // Ci dessous mis en commentaire car ne marche pas. Ajout du de la ligne suivante à la place pour faire fonctionner tel qu'indiqué dans commentaires de la video YT
+      //    (err, docs) => {
+      //          if (!err) return res.send(docs);
+      // if (err) return res.status(500).send({ message: err });
+      //       }
     );
+    return res.status(200).json("L'utilisateur a bien été mis à jour");
   } catch (err) {
-    console.log("titi");
+    return res.status(500).json({ message: err });
+  }
+};
+
+module.exports.deleteUser = async (req, res) => {
+  if (!ObjectID.isValid(req.params.id))
+    return res.status(400).send("ID unkown : " + req.params.id);
+
+  try {
+    await UserModel.remove({ _id: req.params.id }).exec();
+    res.status(200).json({ message: "Successfully deleted." });
+  } catch (err) {
     return res.status(500).json({ message: err });
   }
 };
--- models/user.model.js
+++ models/user.model.js
@@ -17,6 +17,7 @@ const userSchema = new mongoose.Schema(
       required: true,
       validate: [isEmail],
       lowercase: true,
+      unique: true,
       trim: true,
     },
     password: {
@@ -54,6 +55,18 @@ userSchema.pre("save", async function (next) {
   this.password = await bcrypt.hash(this.password, salt);
   next();
 });
+
+userSchema.statics.login = async function (email, password) {
+  const user = await this.findOne({ email });
+  if (user) {
+    const auth = await bcrypt.compare(password, user.password);
+    if (auth) {
+      return user;
+    }
+    throw Error("Incorrect password");
+  }
+  throw Error("Incorrect email");
+};
 
 const userModel = mongoose.model("user", userSchema);
 module.exports = userModel;
